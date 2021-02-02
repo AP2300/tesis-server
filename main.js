86e51e8660244c0ca0d3552fda5e681f4d40c2bd
Andres Paredes <andresparedes202@gmail.com>
2021-02-02 18:01:35 -0400

    ER PRIMERO

---
+++ main.js
@@ -0,0 +1,36 @@
+const   express               = require("express"),
+        app                   = express(),
+        BodyParser            = require("body-parser"),
+        FP                    = require("express-fileupload"),
+        bcrypt                = require("bcryptjs"),
+        NodeMailer            = require("nodemailer")
+        fs                    = require("fs"),
+        { v4: uuidv4 }        = require('uuid');
+
+app.set("port",process.env.PORT||3000);
+app.use(BodyParser.urlencoded({extended:true}));
+app.use(FP());
+app.use(express.static('src'));
+
+app.use(BodyParser.json({'limit':'1mb'}));
+app.disable('x-powered-by');
+
+app.all('*', function(_, res, next){
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, auth, Content-Length, X-Requested-With');
+    next();
+});
+
+let transporter = NodeMailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: "andresparedes202@gmail.com",
+        pass: "rrclmyolimtffmqo"
+    }
+});
+
+app.listen(app.get("port"), function(err){
+    if(err) console.log(err);
+    else console.log("servidor iniciado");  
+});
